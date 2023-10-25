5caaab560c35a4f5609d67967823f3ccfa27ffbd
vinodKumarBhavana <139556763+vinod-centific@users.noreply.github.com>
2023-10-25 11:19:43 +0530

    changes

---
+++ AuthenticationAndAuthorization/AuthenticationAndAuthorization/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using AuthenticationAndAuthorization.DBModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationAndAuthorization.Controllers
+{
+    [Authorize]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        ProductDataContext _context = new ProductDataContext();
+
+
+        [HttpGet] //get
+        [Route("GetAllProducts")]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            return Ok( "success");
+        }
+
+        [HttpGet]
+        [Route("GetProductByCode")]
+        public async Task<IActionResult> GetProductByCode(string ProductCode)
+        {
+            return Ok(_context.TblProducts.Where(e=>e.ProductCode==ProductCode).FirstOrDefault());
+        }
+    }
+}
